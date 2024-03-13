a6ea7f008e00ca292d6e1da449d9b56d59917ca7
Neil Chadwick <neilchadders@hotmail.com>
2024-03-13 08:59:58 +0000

    add productscreen and rating

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -13,12 +13,14 @@ import {
 
 import App from './App';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 );
