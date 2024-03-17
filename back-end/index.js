038550384929881aeca38143848ff96a736bf6ef
Neil Chadwick <neilchadders@hotmail.com>
2024-03-17 14:55:26 +0000

    add config, controllers, middleware, routes, seeder - fetch from Mongo

--- back-end/index.js
+++ back-end/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import products from './data/product.js';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import {notFound, errorHandler } from "./middleware/errorMiddleware.js"
+
 
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -11,14 +16,10 @@ app.get('/', (req, res) => {
     res.send('Api is running...!');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes)
 
+app.use(notFound)
+app.use(errorHandler)
 
-app.get('/api/products/:id', (req,res) => {
-    const product =products.find((p)=> p._id===req.params.id);
-    res.json(product)
-})
 
 app.listen(port, () => console.log(`Server running on ${port}`))
