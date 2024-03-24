678e28a06bd612b60eaa4ddd146c2680ec23e268
Neil Chadwick <neilchadders@hotmail.com>
2024-03-24 17:05:38 +0000

    add backend auth

---
+++ back-end/controllers/userController.js
@@ -0,0 +1,129 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
+
+
+
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+  
+    const user = await User.findOne({ email });
+
+   
+    if (user && (await user.matchPassword(password))) {
+      
+        generateToken(res, user._id);
+          // Set JWT as an HTTP-Only cookie
+          
+
+     
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+    } else {
+      res.status(401);
+      throw new Error('Invalid email or password');
+    }
+  });
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+  
+    const userExists = await User.findOne({ email });
+  
+    if (userExists) {
+      res.status(400);
+      throw new Error('User already exists');
+    }
+  
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+  
+    if (user) {
+      generateToken(res, user._id);
+  
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid user data');
+    }
+  });
+
+// @desc    Logout user / clear cookie
+// @route   POST /api/users/logout
+// @access  Public
+const logoutUser = (req, res) => {
+    res.clearCookie('jwt');
+    res.status(200).json({ message: 'Logged out successfully' });
+  };
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send('get user profile');
+});
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send('update user profile');
+});
+
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  res.send('get users');
+});
+
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send('delete user');
+});
+
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  res.send('get user by id');
+});
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.send('update user');
+});
+
+export {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  logoutUser,
+  deleteUser,
+  getUserById,
+  updateUser,
+};
---
+++ back-end/routes/userRoutes.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  logoutUser,
+  getUserById,
+  updateUser,
+} from '../controllers/userController.js';
+
+const router = express.Router();
+
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+router.route('/').post(registerUser).get(getUsers);
+router.post('/logout', logoutUser);
+router.post('/login', authUser);
+router.route('/profile')
+    
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
+
+
+router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser);
+
+export default router;
