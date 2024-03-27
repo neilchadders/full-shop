8c571b9b86038bd77b90705be4487d5aa9a0c103
Neil Chadwick <neilchadders@hotmail.com>
2024-03-27 06:56:11 +0000

    add front auth

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -18,7 +18,7 @@ import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').post(registerUser).get(getUsers);
 router.post('/logout', logoutUser);
-router.post('/login', authUser);
+router.post('/auth', authUser);
 router.route('/profile')
     
     .get(protect, getUserProfile)
--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice';
 import cartSliceReducer from './slices/cartSlice';// import after created the file
-
+import authSliceReducer from './slices/authSlice';
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducer, // add to configureStoreConst after creation
- 
+    auth:authSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
